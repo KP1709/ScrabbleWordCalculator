89e07a4e4224603da03bb92b4e81191a89a18481
Kareena Patel <kareenapat17@gmail.com>
2024-08-04 09:28:44 +0100

    Updated data type of enteredLetter to 'string'

--- src/reusableFunctions/letterNoTiles.ts
+++ src/reusableFunctions/letterNoTiles.ts
@@ -1,6 +1,6 @@
 import {letters} from "../letterLookup"
 
-export function getLetterNoTiles(enteredLetter: String): number{
+export function getLetterNoTiles(enteredLetter: string): number{
     const usedLetter = enteredLetter.toUpperCase()
     const output = (letters.filter(item => item.letter === usedLetter))
     const maxAmount:number = output[0].maxAmount
--- src/reusableFunctions/letterScore.ts
+++ src/reusableFunctions/letterScore.ts
@@ -1,6 +1,6 @@
 import {letters} from "../letterLookup"
 
-export function getLetterScore(enteredLetter: String): number{
+export function getLetterScore(enteredLetter: string): number{
     const usedLetter = enteredLetter.toUpperCase()
     const output = letters.filter(item => item.letter === usedLetter)
     const tileScore: number = output[0].score
