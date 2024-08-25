3500c8854d6b3d94cec994d8a8aac61deaba4dec
Kareena Patel <kareenapat17@gmail.com>
2024-08-25 17:27:53 +0100

    Checked basic response for invalid input

--- cypress/e2e/mainpage.cy.ts
+++ cypress/e2e/mainpage.cy.ts
@@ -11,6 +11,14 @@ describe('Scrabble Word Calculator', () => {
     cy.getDataTest('start-screen').within(() => {
       cy.get('h2').contains(/Enter a word/i)
     })
-
   })
+
+  // Sad path - invalid input
+  it('Invalid string entered', () => {
+    cy.getDataTest('word-calculator-form').type('gre8t')
+    cy.getDataTest('check-word-button').click()
+    cy.getDataTest('start-screen').should('not.exist')
+    cy.getDataTest('invalid-input-screen').should('exist')
+  })
+
 })
--- src/components/letterCalculator.tsx
+++ src/components/letterCalculator.tsx
@@ -108,7 +108,7 @@ export default function LetterCalculator() {
 
         else if (isValidString === "false") {
             return (
-                <div className="flex-centre">
+                <div className="flex-centre" data-test="invalid-input-screen">
                     <h2>Invalid Entry</h2>
                     <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z" /></svg>
                 </div>
@@ -153,11 +153,12 @@ export default function LetterCalculator() {
                     className="form__input-text"
                     name="word"
                     id="word"
+                    data-test="word-calculator-form"
                     value={wordToCheck}
                     onChange={(e) => setWordToCheck(e.target.value)}
                     placeholder="scrabble"
                 />
-                <input type="submit" value="Check" className="form__input-button" />
+                <input type="submit" value="Check" className="form__input-button" data-test="check-word-button"/>
             </form>
 
             {screenOutput(isValidString, isValidWord)}
