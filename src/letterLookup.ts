ecd8aa3a901e76cebbb48b9159d1a20bb9511ea9
Kareena Patel <kareenapat17@gmail.com>
2024-08-31 11:56:05 +0100

    Reset code to using object than Map

--- src/letterLookup.ts
+++ src/letterLookup.ts
@@ -2,37 +2,36 @@
 
 // Type alias for letters array
 type letterProperties = {
-    letter: string,
     score: number,
     maxAmount: number
 }
 
-export const letters:letterProperties[] = [
-    {"letter":"A", "score":1, "maxAmount":9},
-    {"letter":"B", "score":3, "maxAmount":2},
-    {"letter":"C", "score":3, "maxAmount":2},
-    {"letter":"D", "score":2, "maxAmount":4},
-    {"letter":"E", "score":1, "maxAmount":12},
-    {"letter":"F", "score":4, "maxAmount":2},
-    {"letter":"G", "score":2, "maxAmount":3},
-    {"letter":"H", "score":4, "maxAmount":2},
-    {"letter":"I", "score":1, "maxAmount":9},
-    {"letter":"J", "score":8, "maxAmount":1},
-    {"letter":"K", "score":5, "maxAmount":1},
-    {"letter":"L", "score":1, "maxAmount":4},
-    {"letter":"M", "score":3, "maxAmount":2},
-    {"letter":"N", "score":1, "maxAmount":6},
-    {"letter":"O", "score":1, "maxAmount":8},
-    {"letter":"P", "score":3, "maxAmount":2},
-    {"letter":"Q", "score":10, "maxAmount":1},
-    {"letter":"R", "score":1, "maxAmount":6},
-    {"letter":"S", "score":1, "maxAmount":4},
-    {"letter":"T", "score":1, "maxAmount":6},
-    {"letter":"U", "score":1, "maxAmount":4},
-    {"letter":"V", "score":4, "maxAmount":2},
-    {"letter":"W", "score":4, "maxAmount":2},
-    {"letter":"X", "score":8, "maxAmount":1},
-    {"letter":"Y", "score":4, "maxAmount":2},
-    {"letter":"Z", "score":10, "maxAmount":1},
-    // {"letter":"", "score":0, "maxAmount":2}
-]
+export const letters= new Map<string, letterProperties>([
+    ["A", {"score":1, "maxAmount":9}],
+    ["B", {"score":3, "maxAmount":2}],
+    ["C", {"score":3, "maxAmount":2}],
+    ["D", {"score":2, "maxAmount":4}],
+    ["E", {"score":1, "maxAmount":12}],
+    ["F", {"score":4, "maxAmount":2}],
+    ["G", {"score":2, "maxAmount":3}],
+    ["H", {"score":4, "maxAmount":2}],
+    ["I", {"score":1, "maxAmount":9}],
+    ["J", {"score":8, "maxAmount":1}],
+    ["K", {"score":5, "maxAmount":1}],
+    ["L", {"score":1, "maxAmount":4}],
+    ["M", {"score":3, "maxAmount":2}],
+    ["N", {"score":1, "maxAmount":6}],
+    ["O", {"score":1, "maxAmount":8}],
+    ["P", {"score":3, "maxAmount":2}],
+    ["Q", {"score":10, "maxAmount":1}],
+    ["R", {"score":1, "maxAmount":6}],
+    ["S", {"score":1, "maxAmount":4}],
+    ["T", {"score":1, "maxAmount":6}],
+    ["U", {"score":1, "maxAmount":4}],
+    ["V", {"score":4, "maxAmount":2}],
+    ["W", {"score":4, "maxAmount":2}],
+    ["X", {"score":8, "maxAmount":1}],
+    ["Y", {"score":4, "maxAmount":2}],
+    ["Z", {"score":10, "maxAmount":1}],
+    // ["",{"score":0, "maxAmount":2}]
+])
