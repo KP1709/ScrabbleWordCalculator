7bd753a2bce23911302d812718578c67d85e8c8d
Kareena Patel <kareenapat17@gmail.com>
2024-09-23 17:00:40 +0100

    Clean up code presentation

--- src/components/responseInterfaces.tsx
+++ src/components/responseInterfaces.tsx
@@ -53,15 +53,23 @@ export default function ResponseInterfaces(
             </ul>
             <h3 id="score"> Total : {totalWordScore}</h3>
             <div id="multiplier__buttons" className="flex-centre">
-                {totalWordScoreMultiplier === 1 && <button className="multiplier__button" onClick={() => handleMultiply(2)}>
-                    Double total score
-                </button>}
-                {totalWordScoreMultiplier === 1 && <button className="multiplier__button" onClick={() => handleMultiply(3)}>
-                    Triple total score
-                </button>}
-                {totalWordScoreMultiplier !== 1 && <button className="multiplier__button" onClick={handleReset}>
-                    Reset score multiplier
-                </button>}
+                {totalWordScoreMultiplier === 1 &&
+                    <button className="multiplier__button"
+                        onClick={() => handleMultiply(2)}>
+                        Double total score
+                    </button>}
+
+                {totalWordScoreMultiplier === 1 &&
+                    <button className="multiplier__button"
+                        onClick={() => handleMultiply(3)}>
+                        Triple total score
+                    </button>}
+
+                {totalWordScoreMultiplier !== 1 &&
+                    <button className="multiplier__button"
+                        onClick={handleReset}>
+                        Reset score multiplier
+                    </button>}
             </div>
         </div>
     )
