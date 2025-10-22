e18467ec28b0f8e0db48537edb691d2c17d2941b
Kareena Patel <kareenapat17@gmail.com>
2025-10-22 20:56:26 +0100

    Change SVGs used

--- src/components/responseInterfaces/invalidEntry.tsx
+++ src/components/responseInterfaces/invalidEntry.tsx
@@ -2,7 +2,7 @@ export default function InvalidEntry() {
     return (
         <div className="flex-centre" data-test="invalid-entry-screen">
             <h2>Invalid Entry</h2>
-            <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z" /></svg>
+            <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm88,104a87.56,87.56,0,0,1-20.41,56.28L71.72,60.4A88,88,0,0,1,216,128ZM40,128A87.56,87.56,0,0,1,60.41,71.72L184.28,195.6A88,88,0,0,1,40,128Z"></path></svg>
         </div>
     )
 } 
