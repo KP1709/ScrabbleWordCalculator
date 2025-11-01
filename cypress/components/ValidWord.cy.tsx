f9723fe1a102abb8e03f2489486622aa6697a8ab
Kareena Patel <kareenapat17@gmail.com>
2025-11-01 11:14:06 +0000

    Update test to account for blank tiles

--- cypress/components/ValidWord.cy.tsx
+++ cypress/components/ValidWord.cy.tsx
@@ -34,9 +34,45 @@ describe('<ValidWord  />', () => {
         cy.get("[data-test='list-tile-C']").click()
         cy.get("[data-test='list-tile-C']").click()
         cy.get("[data-test='list-tile-C']").within(() => {
-            cy.get('h2').should('have.css', 'background-color', 'rgb(0, 119, 182)') // #0077b6
-            cy.get('h3').should('contain', 9)
+            cy.get('h2').should('have.css', 'background-color', 'rgb(255, 255, 255)') // #0077b6
+            cy.get('h3').should('contain', 0)
         })
-        cy.get("[data-test='total-word-score']").should('contain', 14)
+
+        cy.get("[data-test='list-tile-T']").click()
+        cy.get("[data-test='list-tile-T']").within(() => {
+            cy.get('h2').should('have.css', 'background-color', 'rgb(0, 119, 182)') // #0077b6
+            cy.get('h3').should('contain', 3)
+        })
+        cy.get("[data-test='total-word-score']").should('contain', 8)
+    })
+
+    it('Testing blank tile functionality', () => {
+        mount(<ValidWord submitWord={true} wordToCheck={'react'} />)
+
+        cy.get("[data-test='list-tile-E']").click()
+        cy.get("[data-test='list-tile-E']").click()
+        cy.get("[data-test='list-tile-E']").click()
+        cy.get("[data-test='list-tile-E']").within(() => {
+            cy.get('h2').should('have.css', 'background-color', 'rgb(255, 255, 255)') // #ffffff
+            cy.get('h3').should('contain', 0)
+        })
+
+        cy.get("[data-test='list-tile-A']").click()
+        cy.get("[data-test='list-tile-A']").click()
+        cy.get("[data-test='list-tile-A']").click()
+        cy.get("[data-test='list-tile-A']").within(() => {
+            cy.get('h2').should('have.css', 'background-color', 'rgb(255, 255, 255)') // #ffffff
+            cy.get('h3').should('contain', 0)
+        })
+
+        cy.get("[data-test='list-tile-R']").click()
+        cy.get("[data-test='list-tile-R']").click()
+        cy.get("[data-test='list-tile-R']").click()
+        cy.get("[data-test='list-tile-R']").within(() => {
+            cy.get('h2').should('have.css', 'background-color', 'rgb(255, 255, 255)') // #ffffff
+            cy.get('h3').should('not.contain', 0)
+        })
+
+        cy.get("[data-test='total-word-score']").should('contain', 5)
     })
 })
