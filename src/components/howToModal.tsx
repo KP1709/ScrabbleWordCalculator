3404f8d657c96c400261c16e2a46390e2a5d18bc
Kareena Patel <kareenapat17@gmail.com>
2025-11-01 17:37:59 +0000

    Update modal instructions

--- src/components/howToModal.tsx
+++ src/components/howToModal.tsx
@@ -17,8 +17,10 @@ export default function HowToModal({ open, onClose }: ModalProps) {
                 <h2>How to use the calculator</h2>
                 <p>Enter a word in the text box and either press enter or the 'check' button</p>
                 <p>If the word contains other characters than letters it will display 'Invalid entry'</p>
+                <p>If the word contains more than 15 letters it will display 'Invalid entry'</p>
                 <p>If the word is not in the dictionary it will display 'Unknown Word'</p>
-                <p>If it contains letters and is definitely a word then the word, letter scores and total word score will be displayed</p>
+                <p>If the word cannot be created based on the number of tiles it will display 'Max Tile Limit Exceeded'</p>
+                <p>If it contains letters and is definitely a word within the rules then the word, letter scores and total word score will be displayed</p>
                 <p>Tapping the tiles will cause letter scores to double, triple, set to 0 and reset back to its original value in this cycle</p>
                 <p>The multiplier buttons below will double or triple the word score</p>
             </div>
