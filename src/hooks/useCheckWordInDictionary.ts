0f38b98cc6ceed25de113ef282f1007d0a2439a7
Kareena Patel <kareenapat17@gmail.com>
2025-11-14 20:08:09 +0000

    Clean up dictionary hook

--- src/hooks/useCheckWordInDictionary.ts
+++ src/hooks/useCheckWordInDictionary.ts
@@ -7,46 +7,56 @@ type CheckInDictionaryType = {
 }
 
 export const useCheckWordInDictionary = ({ wordToCheck, submitWord, setSubmitWord }: CheckInDictionaryType) => {
-    const [isSupabaseError, setIsSupabaseError] = useState<boolean>(false);
-    const [isApiError, setIsApiError] = useState<boolean>(false);
     const [isAnalysing, setIsAnalysing] = useState(false)
     const [isValidWord, setIsValidWord] = useState(false)
     const [isError, setIsError] = useState(false)
 
     useEffect(() => {
         async function checkWord() {
+            setIsAnalysing(true);
+            setIsError(false);
+
+            let apiRes;
+            let shouldRunSupabase = false;
+
             try {
-                setIsAnalysing(true)
-                setIsError(false)
-
-                const apiRes = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToCheck}`)
+                apiRes = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToCheck}`);
                 if (apiRes.ok) {
-                    setIsValidWord(true)
+                    setIsValidWord(true);
+                } else if (apiRes.status === 404) {
+                    shouldRunSupabase = true;
                 }
-                else if (apiRes.status === 404) {
-                    try {
-                        const res = await fetch(`/.netlify/functions/supabase?word=${wordToCheck}`, {
-                            method: "POST",
-                            body: JSON.stringify({ word: wordToCheck }),
-                        })
+                else if (!apiRes.ok && apiRes.status !== 404) {
+                    // Must manually check and specify this 
+                    setIsError(true);
+                    setIsValidWord(false);
+                }
+            } catch (err) {
+                setIsError(true);
+                setIsValidWord(false);
+            } finally {
+                setIsAnalysing(false);
+            }
 
-                        if (res.status === 404) {
-                            setIsValidWord(false)
-                            return
-                        }
-                        const data = await res.json()
-                        data.data[0] ? setIsValidWord(true) : setIsValidWord(false)
-
-                    } catch (err) {
-                        setIsError(true); setIsSupabaseError(true); setIsApiError(true); setIsValidWord(false)
+            if (shouldRunSupabase) {
+                try {
+                    setIsAnalysing(true);
+                    const res = await fetch(`/.netlify/functions/supabase?word=${wordToCheck}`, {
+                        method: "POST",
+                        body: JSON.stringify({ word: wordToCheck }),
+                    });
+                    if (res.status === 404) {
+                        setIsValidWord(false);
+                        return;
                     }
+                    const data = await res.json();
+                    data.data[0] ? setIsValidWord(true) : setIsValidWord(false);
+                } catch (err) {
+                    setIsError(true);
+                    setIsValidWord(false);
+                } finally {
+                    setIsAnalysing(false);
                 }
-            }
-            catch (err) {
-                setIsError(true); setIsApiError(true); setIsValidWord(false)
-            }
-            finally {
-                setIsAnalysing(false)
             }
         }
 
@@ -56,5 +66,5 @@ export const useCheckWordInDictionary = ({ wordToCheck, submitWord, setSubmitWor
         }
     }, [submitWord])
 
-    return { isAnalysing, isApiError, isSupabaseError, isValidWord, isError, wordToCheck }
+    return { isAnalysing, isValidWord, isError, wordToCheck }
 }
