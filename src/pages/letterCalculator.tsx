5b52ace513f6f728b647834678ed24f3def153d7
Kareena Patel <kareenapat17@gmail.com>
2025-11-14 13:34:59 +0000

    Fix instruction button and styling

--- src/pages/letterCalculator.tsx
+++ src/pages/letterCalculator.tsx
@@ -39,25 +39,27 @@ export default function LetterCalculator() {
 
     return (
         <main>
-            <form onSubmit={handleSubmit} className="flex-centre-row">
-                <label htmlFor="word">Word:</label>
-                <input type="text"
-                    className="form__input-text"
-                    name="word"
-                    id="word"
-                    data-test='word-form'
-                    value={wordToCheck}
-                    onChange={(e) => setWordToCheck(e.target.value)}
-                    placeholder="scrabble"
-                />
-                <input type="submit" value="Check" data-test='submit-word-form-btn' className="form__input-button" title='Submit word for check' />
+            <span className="flex-centre-row">
+                <form onSubmit={handleSubmit} className="flex-centre-row">
+                    <label htmlFor="word">Word:</label>
+                    <input type="text"
+                        className="form__input-text"
+                        name="word"
+                        id="word"
+                        data-test='word-form'
+                        value={wordToCheck}
+                        onChange={(e) => setWordToCheck(e.target.value)}
+                        placeholder="scrabble"
+                    />
+                    <input type="submit" value="Check" data-test='submit-word-form-btn' className="form__input-button" title='Submit word for check' />
+                </form>
                 {isValidString !== 'start' && <button id='instructions-btn' onClick={() => setIsHowToModalOpen(true)} title='View instructions'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
                         <path d="M140,180a12,12,0,1,1-12-12A12,12,0,0,1,140,180ZM128,72c-22.06,0-40,16.15-40,36v4a8,8,0,0,0,16,0v-4c0-11,10.77-20,24-20s24,9,24,20-10.77,20-24,20a8,8,0,0,0-8,8v8a8,8,0,0,0,16,0v-.72c18.24-3.35,32-17.9,32-35.28C168,88.15,150.06,72,128,72Zm104,56A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z">
                         </path>
                     </svg>
                 </button>}
-            </form>
+            </span>
             <HowToModal open={isHowToModalOpen} onClose={() => setIsHowToModalOpen(false)} />
 
             {isValidString === "start" && <StartScreen />}
