39e282cf84c9ee3823d6ba83642d4064ea5d5b8c
Kareena Patel <kareenapat17@gmail.com>
2025-11-23 17:04:47 +0000

    Fix settings and help button visibility on mobile

--- src/components/responseInterfaces/startScreen.tsx
+++ src/components/responseInterfaces/startScreen.tsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import HowToModal from "../howToModal";
+import SettingsModal from "../settingsModal";
+import useBreakpoint from "../../hooks/useBreakpoint";
 
 export default function StartScreen() {
-    const [isOpen, setIsOpen] = useState(false)
+    const breakpoint = useBreakpoint()
+    const [isHowToModalOpen, setIsHowToModalOpen] = useState(false)
+    const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false)
 
     return (
         <div className="flex-centre-column response-interface" data-test="start-screen-screen">
             <h2>Enter a word to calculate score</h2>
-            <div>
-                <button className="start__instructions-button" onClick={() => setIsOpen(true)}>View instructions here</button>
-                <HowToModal open={isOpen} onClose={() => setIsOpen(false)} />
+            <div className="flex-centre-column">
+                <button className="start__instructions-button" onClick={() => setIsHowToModalOpen(true)}>View instructions</button>
+                <HowToModal open={isHowToModalOpen} onClose={() => setIsHowToModalOpen(false)} />
+
+                {breakpoint <= 400 && <button className="start__instructions-button" onClick={() => setIsSettingsModalOpen(true)}>Change settings</button>}
+                <SettingsModal open={isSettingsModalOpen} onClose={() => setIsSettingsModalOpen(false)} />
+
             </div>
         </div>
     )
