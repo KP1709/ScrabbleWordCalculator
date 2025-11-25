b713d2a487ee165644428898f55c39ec5595720e
Kareena Patel <kareenapat17@gmail.com>
2025-11-25 20:05:01 +0000

    Make settings and help button better accessible on mobile

--- src/components/responseInterfaces/startScreen.tsx
+++ src/components/responseInterfaces/startScreen.tsx
@@ -1,23 +1,20 @@
 import { useState } from "react";
 import HowToModal from "../howToModal";
-import SettingsModal from "../settingsModal";
 import useBreakpoint from "../../hooks/useBreakpoint";
 
 export default function StartScreen() {
     const breakpoint = useBreakpoint()
     const [isHowToModalOpen, setIsHowToModalOpen] = useState(false)
-    const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false)
 
     return (
         <div className="flex-centre-column response-interface" data-test="start-screen-screen">
             <h2>Enter a word to calculate score</h2>
             <div className="flex-centre-column">
-                <button className="start__instructions-button" onClick={() => setIsHowToModalOpen(true)}>View instructions</button>
-                <HowToModal open={isHowToModalOpen} onClose={() => setIsHowToModalOpen(false)} />
-
-                {breakpoint <= 450 && <button className="start__instructions-button" onClick={() => setIsSettingsModalOpen(true)}>Change settings</button>}
-                <SettingsModal open={isSettingsModalOpen} onClose={() => setIsSettingsModalOpen(false)} />
-
+                {breakpoint >= 450 &&
+                    <>
+                        <button className="start__instructions-button" onClick={() => setIsHowToModalOpen(true)}>View instructions</button>
+                        <HowToModal open={isHowToModalOpen} onClose={() => setIsHowToModalOpen(false)} />
+                    </>}
             </div>
         </div>
     )
