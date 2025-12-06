1f1b3cf48f147aea66c12c2f608496580bdfdd14
Kareena Patel <kareenapat17@gmail.com>
2025-12-06 16:14:09 +0000

    Always show mobile bar + remove svgs next to search bar

--- src/components/responseInterfaces/startScreen.tsx
+++ src/components/responseInterfaces/startScreen.tsx
@@ -1,21 +1,7 @@
-import { useState } from "react";
-import HowToModal from "../modals/howToModal";
-import useBreakpoint from "../../hooks/useBreakpoint";
-
 export default function StartScreen() {
-    const breakpoint = useBreakpoint()
-    const [isHowToModalOpen, setIsHowToModalOpen] = useState(false)
-
     return (
         <div className="flex-centre-column response-interface" data-test="start-screen-screen">
             <h2>Enter a word to calculate score</h2>
-            <div className="flex-centre-column">
-                {breakpoint >= 450 &&
-                    <>
-                        <button className="start__instructions-button" onClick={() => setIsHowToModalOpen(true)}>View instructions</button>
-                        <HowToModal open={isHowToModalOpen} onClose={() => setIsHowToModalOpen(false)} />
-                    </>}
-            </div>
         </div>
     )
 }
