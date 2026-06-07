d13980e45fd24dc2cfaf965215b7e1aaffe80c9a
Kareena Patel <kareenapat17@gmail.com>
2026-06-07 21:13:31 +0100

    Improve focus accessibility

--- src/components/tile.tsx
+++ src/components/tile.tsx
@@ -5,9 +5,14 @@ type TileProp = LetterProperties & { onClick: () => void; };
 
 const Tile = ({ letter, score, onClick, colour }: TileProp) => {
     return (
-        <div onClick={onClick} data-test={`list-tile-${letter}`}
+        <div data-test={`list-tile-${letter}`}
         >
-            <h2 className={`flex-centre-column ${colour}`}>{letter}</h2>
+            <h2
+                className={`flex-centre-column ${colour}`}
+                tabIndex={0} onClick={onClick}
+                onKeyDown={(e) => { if (e.key === ' ') onClick(); }}>
+                {letter}
+            </h2>
             <h3 className="flex-centre-column">{score}</h3>
         </div>
     );
