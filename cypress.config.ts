dbf740607cdab74dc74a4af0e4fc2ea35bb6bb8c
Kareena Patel <kareenapat17@gmail.com>
2026-09-13 14:17:00 +0100

    Update Cypress config to use chrome as default browser

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
+    defaultBrowser: 'chrome',
     devServer: {
       framework: "react",
       bundler: "vite",
@@ -10,6 +11,7 @@ export default defineConfig({
 
   e2e: {
     baseUrl: "http://localhost:5173/",
+    defaultBrowser: 'chrome',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
